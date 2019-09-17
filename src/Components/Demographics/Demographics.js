8afb0a4036dfc923279094cbfa72f2a37c10d345
Jengopockets <jengopockets@gmail.com>
2019-09-17 13:19:00 -0400

    Sex Field

--- src/Components/Demographics/Demographics.js
+++ src/Components/Demographics/Demographics.js
@@ -3,13 +3,14 @@ import {Link} from 'react-router-dom';
 
 const Demogrphics= props => {
 
-    const [state, setState] = useState({selectedOption:'18-24'})
+    const [age, setAge] = useState({selectedOption:'18-24'})
+    const [sex, setSex] = useState({selectedOption:'Male'})
 
     const changeHandler = e => {
         e.persist();
         let value = e.target.value;
 
-        setState({
+        setAge({
             selectedOption: e.target.value
         });
     }
@@ -21,20 +22,35 @@ const Demogrphics= props => {
             <div className="ageField">
                 <h2>Age</h2>
                     <label>
-                        <input type="radio" value="18-24" checked={state.selectedOption === '18-24'} onChange={changeHandler} />
+                        <input type="radio" value="18-24" checked={age.selectedOption === '18-24'} onChange={changeHandler} />
                         18-24
                     </label>
                     <label>
-                        <input type="radio" value="25-30" checked={state.selectedOption === '25-30'} onChange={changeHandler} />
+                        <input type="radio" value="25-30" checked={age.selectedOption === '25-30'} onChange={changeHandler} />
                         25-30
                     </label>
                     <label>
-                        <input type="radio" value="31-55" checked={state.selectedOption === '31-55'} onChange={changeHandler} />
+                        <input type="radio" value="31-55" checked={age.selectedOption === '31-55'} onChange={changeHandler} />
                         31-55
                     </label>
                     <label>
-                        <input type="radio" value="55 +" checked={state.selectedOption === '55 +'} onChange={changeHandler} />
+                        <input type="radio" value="55 +" checked={age.selectedOption === '55 +'} onChange={changeHandler} />
                         55 +
+                    </label>
+            </div>
+            <div className="sexField">
+                <h2>Sex</h2>
+                    <label>
+                        <input type="radio" value="Male" checked={sex.selectedOption === 'Male'} onChange={changeHandler} />
+                        Male
+                    </label>
+                    <label>
+                        <input type="radio" value="Female" checked={sex.selectedOption === 'Female'} onChange={changeHandler} />
+                        Female
+                    </label>
+                    <label>
+                        <input type="radio" value="Other" checked={sex.selectedOption === 'Other'} onChange={changeHandler} />
+                        Other
                     </label>
             </div>
         </div>
