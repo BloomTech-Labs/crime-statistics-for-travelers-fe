452892472bc5d5d15c29bc85f29fd117bb6514f5
umbur <you@example.com>
2019-09-18 17:05:40 -0600

    1st part of middle content done

--- src/Components/About/About.js
+++ src/Components/About/About.js
@@ -1,6 +1,7 @@
 import React from "react";
 import '../About/about.css';
 import map from '../../assets/web/about/map.png'
+import travelglobe from '../../assets/web/about/travelglobe.png';
 
 function About() {
     return(
@@ -22,6 +23,18 @@ function About() {
                     Start Your Search
                 </div>
                 <input type='text' size="50" placeholder='Zip code'></input>
+            </div>
+        </div>
+        <div className='middleContent'>
+            <div className='middleText'>
+                Making it Safe to Go<br/>
+                from One Place to <br/>Another 
+            </div>
+            <div className='middleText1'>
+                Travel safe makes it convenient to learn about crime in the<br/> places you're headed to.
+            </div>
+            <div className='travelGlobe'>
+                <img alt="" src={travelglobe} /> 
             </div>
         </div>
         </>
