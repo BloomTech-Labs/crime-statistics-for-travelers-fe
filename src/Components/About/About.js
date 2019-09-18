36af2a64b4ed042180db25daa37001a726c31f18
umbur <you@example.com>
2019-09-18 11:06:42 -0600

    created and styled navigation in About page

--- src/Components/About/About.js
+++ src/Components/About/About.js
@@ -1,11 +1,16 @@
 import React from "react";
-
+import '../About/about.css';
 
 function About() {
     return(
-        <>
-        Hello I am About page!
-        </>
+        <header class="header">
+            <ul class="main-nav">
+                <li><a href="#">MAP</a></li>
+                <li><a href="#">ABOUT THE DATA</a></li>
+                <li><a href="#">WHO WE SERVE</a></li>
+                <li className='login'><a href="#">LOGIN</a></li>
+            </ul>
+        </header>
     )
 }
 
