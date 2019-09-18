8fdbcf0f970478f75b47026b3750d4365d2bb3f8
umbur <you@example.com>
2019-09-18 14:00:05 -0600

    apllied and styled map and input field

--- src/Components/About/About.js
+++ src/Components/About/About.js
@@ -1,8 +1,10 @@
 import React from "react";
 import '../About/about.css';
+import map from '../../assets/web/about/map.png'
 
 function About() {
     return(
+        <>
         <header class="header">
             <ul class="main-nav">
                 <li><a href="#">MAP</a></li>
@@ -11,6 +13,18 @@ function About() {
                 <li className='login'><a href="#">LOGIN</a></li>
             </ul>
         </header>
+        <div className='mainMap'>
+        <div className='image'>
+            <img width={1500} height={500} alt="" src={map} /> 
+        </div>
+            <div className='search'>
+                <div className='searchText'>
+                    Start Your Search
+                </div>
+                <input type='text' size="50" placeholder='Zip code'></input>
+            </div>
+        </div>
+        </>
     )
 }
 
