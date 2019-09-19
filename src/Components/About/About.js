1d81ed5ed96354a1965be16f65733653621d7c8e
umbur <you@example.com>
2019-09-19 10:01:04 -0600

    2nd part of the middle content done

--- src/Components/About/About.js
+++ src/Components/About/About.js
@@ -2,6 +2,8 @@ import React from "react";
 import '../About/about.css';
 import map from '../../assets/web/about/map.png'
 import travelglobe from '../../assets/web/about/travelglobe.png';
+import movers from '../../assets/web/about/movers.png';
+import travel from '../../assets/web/about/travel.png';
 
 function About() {
     return(
@@ -37,7 +39,32 @@ function About() {
                 <img alt="" src={travelglobe} /> 
             </div>
         </div>
-        </>
+        <div className='middleContent2'>
+            <div className='movers'>
+                <img alt="" src={movers} /> 
+            </div>
+            <div className='greyBox'>
+                <div className='greyHeader'>
+                    The App
+                </div>
+                <div className='greyHeader2'>
+                    What TravelSafe Does 
+                </div>
+                <div className='greyText'>
+                    TravelSafe has two groups in mind, travelers, and people planning to move. TS<br/> 
+                    provides those people with an easy way to look up a certain place and find out <br/>
+                    what the crime stats are like. Users can filter out crimes they need to know more<br/> 
+                    about - violent, theft, property damage, etc. Registered users can provide TS<br/> 
+                    with demographic data which is put through our proprietary data model to find<br/> 
+                    out which crimes those users are likeliest to be victims of. This can help those<br/> 
+                    people make safe travel and moving plans tailored specifically for them.
+                </div>
+            </div>
+            <div className='travelPic'>
+                <img alt="" src={travel} /> 
+            </div>
+        </div>
+    </>
     )
 }
 
