295df332c2e7a0af5667dc7346e08235be720ced
umbur <you@example.com>
2019-09-23 13:01:24 -0600

    added functionality to submit/post data to the api

--- src/Components/Contact/Contact.js
+++ src/Components/Contact/Contact.js
@@ -1,7 +1,40 @@
 import React from 'react';
 import '../Contact/contact.css';
+import axios from 'axios';
 
-function Contact() {
+class Contact extends React.Component {
+    constructor(props) {
+      super(props)
+      this.state = {
+          name : '',
+          phone: '',
+          title: '',
+          email: '',
+          text : '',
+          requestsuccess: 'false'
+      }  
+    }
+
+    handleSubmit = () => {
+    //console.log('handleSubmit fired!')
+      axios
+        .post('https://reqres.in/api/users', {name: this.state.name, phone: this.state.phone, title: this.state.title, email: this.state.email, text: this.state.text })
+        .then(res => {
+          this.setState({name: res.data.name, phone: res.data.phone, title: res.data.title, email: res.data.email,  text: res.data.text })
+          console.log(res);
+          this.setState({requestsuccess: true})
+          this.setState({name: '', phone: '', title: '', email: '',  text: '' })
+          alert("Thanks for contacting us!");
+        })
+        .catch(err => console.log(err));
+    }
+
+    handleChanges = e => {
+        this.setState( { [e.target.name] : e.target.value } )
+    }
+
+
+render() {
     return(
         <>
         <header class="header">
@@ -30,35 +63,36 @@ function Contact() {
                 Name
             </div>
             <div className='inputName'>
-                <input type='text' size='110'></input>
+                <input type='text' onChange={this.handleChanges} value={this.state.name} name='name' size='110' ></input>
             </div>
             <div className='phoneForm'>
                 Phone
             </div>
             <div className='inputPhone'>
-                <input type='text' size='110'></input>
+                <input type='text' onChange={this.handleChanges} value={this.state.phone} name='phone' size='110'></input>
             </div>
             <div className='titleForm'>
                 Title
             </div>
             <div className='inputTitle'>
-                <input type='text' size='110'></input>
+                <input type='text' onChange={this.handleChanges} value={this.state.title} name='title' size='110'></input>
             </div>
             <div className='emailForm'>
                 Email
             </div>
             <div className='inputEmail'>
-                <input type='text' size='110'></input>
+                <input type='text' onChange={this.handleChanges} value={this.state.email} name='email' size='110'></input>
             </div>
             <div className='blueForm'></div>
             <div className='inputContact'>
-            <textarea rows="12" cols="109" placeholder="What Can We Do For You?" spellcheck="false"></textarea>
+            <textarea onChange={this.handleChanges} value={this.state.text} name="text"  rows="12" cols="109" placeholder="What Can We Do For You?"></textarea>
             </div>
-            <button className='submitButton'>SUBMIT</button>
+            <button onClick={this.handleSubmit} className='submitButton'>SUBMIT</button>
             <div className='spaceArea'></div>
     </div> 
 </>
 )
+ }
 }
 
 export default Contact;
