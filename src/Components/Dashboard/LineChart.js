d9141ae44c7171d8f56a6756051bd9f2e5cf9ba2
Bryce-Soghigian <brybrycodingguy@gmail.com>
2019-09-24 17:24:49 -0500

    adding dashboard style

--- src/Components/Dashboard/LineChart.js
+++ src/Components/Dashboard/LineChart.js
@@ -51,8 +51,8 @@ export default class Example extends PureComponent {
         <YAxis />
         <Tooltip />
         <Legend />
-        <Line type="monotone" dataKey="pv" stroke="#FF0000" activeDot={{ r: 8 }} />
-        <Line type="monotone" dataKey="uv" stroke="#228b22" />
+        <Line type="monotone" dataKey="pv" stroke="#39ff14" activeDot={{ r: 8 }} />
+        <Line type="monotone" dataKey="uv" stroke="#FFC0CB" />
       </LineChart>
     );
   }
--- src/Components/Dashboard/MainDashboard.js
+++ src/Components/Dashboard/MainDashboard.js
@@ -7,7 +7,8 @@ const Container = styled.div`
 width:100%;
 height:100%;
 
-background:whitesmoke;
+background-image:linear-gradient(100deg, rgba(79, 79, 79, 0.07) 0%, rgba(79, 79, 79, 0.07) 50%,rgba(131, 131, 131, 0.07) 50%, rgba(131, 131, 131, 0.07) 100%),linear-gradient(88deg, rgba(197, 197, 197, 0.03) 0%, rgba(197, 197, 197, 0.03) 50%,rgba(184, 184, 184, 0.03) 50%, rgba(184, 184, 184, 0.03) 100%),linear-gradient(29deg, rgba(211, 211, 211, 0.03) 0%, rgba(211, 211, 211, 0.03) 50%,rgba(4, 4, 4, 0.03) 50%, rgba(4, 4, 4, 0.03) 100%),linear-gradient(162deg, rgba(128, 128, 128, 0.1) 0%, rgba(128, 128, 128, 0.1) 50%,rgba(43, 43, 43, 0.1) 50%, rgba(43, 43, 43, 0.1) 100%),linear-gradient(221deg, rgba(251, 251, 251, 0.02) 0%, rgba(251, 251, 251, 0.02) 50%,rgba(191, 191, 191, 0.02) 50%, rgba(191, 191, 191, 0.02) 100%),linear-gradient(176deg, rgba(74, 74, 74, 0.07) 0%, rgba(74, 74, 74, 0.07) 50%,rgba(198, 198, 198, 0.07) 50%, rgba(198, 198, 198, 0.07) 100%),linear-gradient(236deg, rgba(234, 234, 234, 0.03) 0%, rgba(234, 234, 234, 0.03) 50%,rgba(72, 72, 72, 0.03) 50%, rgba(72, 72, 72, 0.03) 100%),linear-gradient(115deg, rgba(87, 87, 87, 0.08) 0%, rgba(87, 87, 87, 0.08) 50%,rgba(145, 145, 145, 0.08) 50%, rgba(145, 145, 145, 0.08) 100%),linear-gradient(90deg, rgb(23, 169, 215),rgb(236, 81, 93));
+
 display:flex;
 align-items:center;
 justify-content:space-evenly;
