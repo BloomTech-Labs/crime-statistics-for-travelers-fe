2939f7fd5fae7cbd37d212f3b2f3c6ffaff82e1f
umbur <you@example.com>
2019-09-30 14:13:50 -0600

    Created average safety percent rate, edited/added some info

--- src/Components/Metadata/CountrySafety.js
+++ src/Components/Metadata/CountrySafety.js
@@ -1,9 +1,10 @@
 import React,{useEffect,useState} from 'react';
 import axios from 'axios';
-import fakedata from '../Map/data/fakedata.json'
+import fakedata from '../Map/data/fakedata.json';
 
 function CountrySafety(){
     const [safety, setSafety] = useState(0);
+    const [safetyPercent, setSafetyPercent] = useState(0);
     
     useEffect(() =>{
         let safetyRating = 0;
@@ -12,12 +13,16 @@ function CountrySafety(){
         })
         safetyRating = safetyRating / fakedata.features.length;
         setSafety(safetyRating)
+        let usSafetyPercent = safetyRating * 100 / 100000 
+        setSafetyPercent(usSafetyPercent)
     },[])
     
     return (
         <div>
-            <h1>Average safety rate of The United States</h1>
-            <p>{safety}</p>
+            <h1>Average safety rate of the United States</h1>
+            <p>{safety} per 100,000</p>
+            <h1>Average safety percent rate of the United States</h1> 
+            <p>{safetyPercent}% per 100,000</p> 
         </div>
     )
 }
