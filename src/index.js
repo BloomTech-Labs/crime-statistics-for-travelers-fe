a2511ee334e76b81e180a8b41088eea08142c9cc
Richard Scott Prins Jr <richardsprins@gmail.com>
2019-10-02 17:32:26 -0400

    uncommenting sentry

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import * as Sentry from '@sentry/browser';
 import './index.css';
 import App from "./App"
  
-// Sentry.init({dsn: "https://8b1b483a6bfd4f25bcf93cf3be6c960d@sentry.io/1761396"})
+Sentry.init({dsn: "https://8b1b483a6bfd4f25bcf93cf3be6c960d@sentry.io/1761396"})
 ReactDOM.render(<BrowserRouter><App/></BrowserRouter>, document.getElementById('root'));
 
 
