f562a515e640403e94918ab4644650fdab95800f
Bryce-Soghigian <brybrycodingguy@gmail.com>
2019-10-07 02:56:34 -0500

    adding ori data with time querys

---
+++ src/Components/Filter/AgencyData.js
@@ -0,0 +1,166 @@
+import React,{useEffect,useState} from 'react'
+import axios from 'axios';
+import styled from 'styled-components';
+
+const PrettyDiv = styled.div`
+    border: 1px solid lightgrey;
+    border-radius: 10px;
+    padding: 5px;
+    margin: 10px auto;
+    box-shadow: 11px 11px 12px -10px rgba(217,217,217,1);
+
+`
+export default function Filter() {
+
+    const [state, setState] = useState()
+    const [offense,setOffense] = useState();
+    const [stateAbbr,setStateAbbr]= useState();
+    const [since,setSince] = useState();
+    const [until,setUntil] = useState();
+    
+const handleSince = (event) => {
+    setSince(parseInt(event.target.value))
+} 
+const handleUntil = (event) => {
+    setUntil(parseInt(event.target.value))
+}
+
+    function handleOffense(event) {
+        setOffense(event.target.value )
+
+    }
+    
+    function handleStateAbbr(event) {
+      setStateAbbr( event.target.value)
+
+    }
+    console.log(offense);
+    console.log(stateAbbr);
+useEffect(() => {
+    console.log(`${offense}`,"API");
+    //https://api.usa.gov/crime/fbi/sapi/api/summarized/state/${stateAbbr}/${offense}/${since}/${until}?API_KEY=iiHnOKfno2Mgkt5AynpvPpUQTEyxE77jo1RU8PIv
+    axios.get(`https://api.usa.gov/crime/fbi/sapi/api/summarized/state/${stateAbbr}/${offense}/${since}/${until}?API_KEY=iiHnOKfno2Mgkt5AynpvPpUQTEyxE77jo1RU8PIv`)
+    .then((res) => {
+        let data = (res.data.data);
+
+        let currentData = data.filter((cD) => {
+            return parseInt(cD.data_year) > 2010;
+          });
+        let newData = currentData.map( nD => {
+
+            return(<PrettyDiv> <p>{nD.value + " Instances of "+offense +" occured in the year " + nD.data_year+ " within the state "+stateAbbr}</p></PrettyDiv>)
+        });
+        setState(newData)
+        console.log("filter res.data", newData)
+
+    }).catch((err) => {
+        console.error(err)
+    })
+
+
+// console.log("state",state)
+    },[offense,stateAbbr,until,since])
+
+
+
+
+
+    return (
+        <div >
+<select id="Offense" onChange={handleOffense} >
+    <optgroup label="Offense" >
+
+  <option>Select Crime</option>
+
+  <option value="arson" >Arson</option>
+  <option value="rape">Rape</option>
+  <option value="motor-vehicle-theft">Vehicle Theft</option>
+ <option value="burglary">Burglary</option>
+  <option value="aggravated-assault">aggravated assault</option>
+  <option value="robbery">Robbery</option>
+  <option value="violent-crime">Violent-Crime</option>
+  <option value="larceny">Larceny</option>
+  <option value="homicide">Homicide</option>
+  <option value="property-crime">Property-Crime</option>
+  </optgroup>
+</select>
+
+<select id="StateAbbr" onChange={handleStateAbbr}>
+    <optgroup label="State">
+
+<option>Select State</option>
+
+<option value="AL">Alabama</option>
+{/* <option value="AK">Alaska</option> */}
+<option value="AZ">Arizona</option>
+<option value="AR">Arkansas</option>
+{/* <option value="CA">California</option> */}
+<option value="CO">Colorado</option>
+<option value="CT">Connecticut</option>
+<option value="DE">Delaware</option>
+ <option value="FL">Florida</option>
+<option value="GA">Georgia</option>
+<option value="HI">Hawaii</option>
+<option value="ID">Idaho</option>
+<option value="IL">Illinois</option>
+<option value="IN">Indiana</option>
+<option value="IA">Iowa</option>
+<option value="KS">Kansas</option>
+<option value="KY">Kentucky</option>
+<option value="LA">Louisiana</option>
+<option value="ME">Maine</option>
+<option value="MD">Maryland</option>
+<option value="MA">Massachusetts</option>
+<option value="MI">Michigan</option>
+<option value="MN">Minnesota</option>
+<option value="MS">Mississippi</option>
+<option value="MO">Missouri</option>
+<option value="MT">Montana</option>
+<option value="NE">Nebraska</option>
+{/* <option value="NV">Nevada</option> */}
+<option value="NH">New Hampshire</option>
+{/* <option value="NJ">New Jersey</option> */}
+<option value="NM">New Mexico</option>
+{/* <option value="NY">New York</option> */}
+{/* <option value="NC">North Carolina</option> */}
+<option value="ND">North Dakota</option>
+<option value="OH">Ohio</option>
+<option value="OK">Oklahoma</option>
+<option value="OR">Oregon</option>
+<option value="PA">Pennsylvania</option>
+<option value="RI">Rhode Island</option>
+<option value="SC">South Carolina</option>
+<option value="SD">South Dakota</option>
+<option value="TN">Tennessee</option>
+<option value="TX">Texas</option>
+<option value="UT">Utah</option>
+<option value="VT">Vermont</option>
+<option value="VA">Virginia</option>
+<option value="WA">Washington</option>
+<option value="WV">West Virginia</option>
+<option value="WI">Wisconsin</option>
+{/* <option value="WY">Wyoming</option> */}
+
+</optgroup>
+</select>
+<optgroup label="Since">
+    <select id="since" onChange={handleSince}>
+    <option value="2015">2015</option>
+        <option value="2016">2016</option>
+        <option value="2017">2017</option>
+        <option value="2018">2018</option>
+       
+    </select>
+</optgroup>
+<optgroup label="Until">
+    <select id="until" onChange={handleUntil}></select>
+<option value="2016">2016</option>
+<option value="2017">2017</option>
+<option value="2018">2018</option>
+</optgroup>
+            <ul>{state}</ul>
+
+
+        </div>
+    )
+}
