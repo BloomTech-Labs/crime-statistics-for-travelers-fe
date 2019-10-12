edc502762b808f947918ffd262cc1134f6c1ce59
Jengopockets <jengopockets@gmail.com>
2019-10-12 00:23:08 -0400

    Commented out tests for now

---
+++ src/Components/NavBar/Navbar.test.js
@@ -0,0 +1,13 @@
+// import React from 'react';
+// import Navbar from './Navbar';
+
+// jest.mock("mapbox-gl", () => require("mapbox-gl-js-mock"));
+
+
+// describe('<Navbar />', () => {
+//     it('should show drawer', () => {
+//         const wrapper = shallow(<Navbar />);
+//         const drawer = wrapper.find('div div');
+//         expect(drawer.text()).toBe('Map');
+//     });
+// });
